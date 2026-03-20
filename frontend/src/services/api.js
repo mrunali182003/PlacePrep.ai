a63d7facca23754cccb123777aaa12bada89eac0
mrunali182003 <mrunalideshmukh182003@gmail.com>
2026-03-21 01:03:50 +0530

    Add production API URL support

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000'
+})
 
 // Auto-attach token to every request
 API.interceptors.request.use((req) => {
